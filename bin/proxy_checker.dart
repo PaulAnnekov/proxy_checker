bf79e383a94c689e93a2eaf72329a9233a961dd0
Paul Annekov <paul.annekov@gmail.com>
2015-02-04 13:55:28 +0200

    Init.

---
+++ bin/proxy_checker.dart
@@ -0,0 +1,61 @@
+/**
+ * 1. Collect proxy ips. Only anonymous (check on server).
+ * 2. Get categories list. Single request.
+ * 3. Get list of products. Pager.
+ * 4. Get list of reviews. Pager.
+ */
+
+import "dart:io";
+import "package:args/args.dart";
+import "package:logging/logging.dart";
+import "package:duct_tape/isolate_wrapper.dart";
+import "package:proxy_checker/proxy_checker.dart";
+
+main(List<String> args) async {
+  Logger.root.level = Level.INFO;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+
+  final Logger log = new Logger('main');
+
+  ArgParser argParser = new ArgParser();
+  argParser.addOption('proxy-list', abbr: 'p', defaultsTo: 'proxy.txt',
+      help: 'Collect from Google search results: ":8080"  ":3128"  ":80" filetype:txt');
+  argParser.addOption('proxy-valid', abbr: 'v', defaultsTo: 'proxy-valid.txt');
+  argParser.addOption('host-ip', abbr: 'i');
+  ArgResults results = argParser.parse(args);
+
+  File proxyFile = new File(results['proxy-list']);
+  if(!proxyFile.existsSync()) {
+    log.severe('File ${results['proxy-list']} with proxy list does not'
+      ' exist.');
+    return;
+  }
+
+  File validFile = new File(results['proxy-valid']);
+  if (validFile.existsSync())
+    validFile.deleteSync();
+
+  List<String> proxyIPs = proxyFile.readAsLinesSync();
+
+  IsolatesController isolatesController = new IsolatesController();
+  isolatesController.spawn(new ProxyChecker(results['host-ip']));
+  isolatesController.spawn(new ProxyChecker(results['host-ip']));
+  isolatesController.spawn(new ProxyChecker(results['host-ip']));
+  isolatesController.spawn(new ProxyChecker(results['host-ip']));
+  isolatesController.listen((message) {
+    log.finest('Message from isolate: "$message"');
+
+    if (message == 'getIP') {
+      if (!proxyIPs.isEmpty)
+        return proxyIPs.removeAt(0);
+
+      log.info('Scanning done');
+    } else if ((message as String).contains(':')) {
+      validFile.writeAsString(message+'\n', mode: FileMode.APPEND);
+    } else {
+      log.warning('Unknown message: "$message"');
+    }
+  });
+}
---
+++ lib/proxy_checker.dart
@@ -0,0 +1,61 @@
+library rozetka_email_dumper;
+
+import "dart:io";
+import "dart:convert";
+import "package:logging/logging.dart";
+import "package:duct_tape/isolate_wrapper.dart";
+
+class ProxyChecker implements IsolateWrapper {
+  IsolateSpawned _isolate;
+  HttpClient _client;
+  Logger _log;
+  String _hostIP;
+
+  ProxyChecker(this._hostIP);
+
+  run(IsolateSpawned isolate) async {
+    Logger.root.level = Level.INFO;
+    Logger.root.onRecord.listen((LogRecord rec) {
+      print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    });
+
+    _isolate = isolate;
+    _client = new HttpClient();
+    _log = new Logger('proxy-checker');
+
+    _checkProxy(await _isolate.send('getIP'));
+  }
+
+  _checkProxy(String proxy) async {
+    if (proxy == null)
+      return;
+
+    _log.info('Check $proxy');
+
+    _client.findProxy = (Uri url) => 'PROXY $proxy';
+
+    _client.getUrl(Uri.parse('http://www.xhaus.com/headers'))
+        .timeout(const Duration(seconds: 5))
+        .then((HttpClientRequest request) => request.close())
+        .timeout(const Duration(seconds: 5))
+        .then((HttpClientResponse response) {
+          response.transform(UTF8.decoder).listen((String body) {
+            if(!body.contains('Your browser software transmitted the '
+            'following HTTP headers') || body.contains(_hostIP))
+              return;
+
+            _log.info('$proxy is valid');
+
+            _isolate.send(proxy);
+          }).onError(() {
+            // Do nothing.
+          });
+        })
+        .catchError((e) {
+          _log.fine('Error $e for $proxy');
+        })
+        .whenComplete(() async {
+          _checkProxy(await _isolate.send('getIP'));
+        });
+  }
+}
